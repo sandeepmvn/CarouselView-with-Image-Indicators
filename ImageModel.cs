160ecf0bb36181f5dd4016f8240c74f97c6a18db
Sandeep <sanday1994@gmail.com>
2018-06-05 10:56:25 +0530

    Carousel View Main Files

---
+++ ImageModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselViewApp
+{
+    public class ImageModel
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+
+        public List<ImageModel> GetData()
+        {
+            return new List<ImageModel>() {
+           new ImageModel() { Name = "one", FullPath = "one.jpg" },
+           new ImageModel() { Name = "two", FullPath = "two.jpeg" },
+           new ImageModel() { Name = "three", FullPath = "three.jpeg" },
+           new ImageModel() { Name = "four", FullPath = "four.jpg" },
+           new ImageModel() { Name = "five", FullPath = "five.jpg" },
+           new ImageModel() { Name = "six", FullPath = "six.jpg" },
+           new ImageModel() { Name = "seven", FullPath = "seven.jpg" }
+            };
+        }
+
+    }
+}
---
+++ MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CarouselViewApp
+{
+   public class MainPageViewModel:INotifyPropertyChanged
+    {
+        private List<ImageModel> _lstImage;
+        int _position;
+
+        //binding models
+        public List<ImageModel> lstImage { get => _lstImage; set { _lstImage = value;onPropertyChanged(); } }
+        public int position { get => _position; set { _position = value; onPropertyChanged(); } }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void onPropertyChanged([CallerMemberName]string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public MainPageViewModel() {
+            position = 0;
+            img_clicked = new Command(ImageClicked);
+            lstImage = new ImageModel().GetData();
+        }
+
+
+        public ICommand img_clicked { get; }
+
+        public void ImageClicked(object image)
+        {
+            var path = (image as Xamarin.Forms.Image).Source.ToString().Split(':')[1].Trim();
+            position = lstImage.IndexOf(lstImage.Find(x => x.FullPath == path));
+        }
+    }
+}
